312b33acaa13605e7406186fc3ca633f3c376e32
jeancgarciaq <jcvictory@hotmail.com>
2025-07-19 23:29:06 -0400

    feat: Agregue dato filiable user_id y relación con User

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+
 
 class Task extends Model
 {
     use HasFactory;
 
     // Permite la asignación masiva
-    protected $fillable = ['title', 'description'];
+    protected $fillable = ['title', 'description', 'user_id'];
+
+    /**
+     * Get the user that owns the task.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
