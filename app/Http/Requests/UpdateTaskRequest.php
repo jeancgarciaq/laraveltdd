fce225a3e0cc29d4da80b83a8fa33efcd806de30
jeancgarciaq <jcvictory@hotmail.com>
2025-07-20 16:49:17 -0400

    add: UpdateTaskRequest

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        // La autorización para actualizar se maneja en el controlador con el middleware 'can'.
+        // Aquí solo nos preocupamos si el usuario está autenticado.
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title field is required.',
+            'title.max' => 'The title may not be greater than 255 characters.',
+        ];
+    }
+}
