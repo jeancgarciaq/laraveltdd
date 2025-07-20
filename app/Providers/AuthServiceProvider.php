34fab60fea5acd6524cac534c357f32e72b1b63c
jeancgarciaq <jcvictory@hotmail.com>
2025-07-19 23:35:04 -0400

    add: AuthServiceProvider agregado para que funcione las políticas

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate; // Podrías necesitar esto para Gates
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        \App\Models\Task::class => \App\Policies\TaskPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Define your route model bindings, pattern filters, authentication callbacks, etc.
+     */
+    public function boot(): void
+    {
+        // Opcionalmente, puedes definir gates aquí si no usas políticas
+        // Gate::define('update-post', function (User $user, Post $post) {
+        //     return $user->id === $post->user_id;
+        // });
+    }
+}
